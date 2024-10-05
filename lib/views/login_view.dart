1b24b158c2f570462a2f99d67cc054e92b9b6df1
Ilija Bunche <ibunceski2003@gmail.com>
2024-10-05 21:42:41 +0200

    step-24

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -5,6 +5,7 @@ import 'package:notesapp/services/auth/auth_exceptions.dart';
 import 'package:notesapp/services/auth/auth_service.dart';
 import 'package:notesapp/services/auth/bloc/auth_bloc.dart';
 import 'package:notesapp/services/auth/bloc/auth_event.dart';
+import 'package:notesapp/services/auth/bloc/auth_state.dart';
 import 'package:notesapp/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -54,24 +55,29 @@ class _LoginViewState extends State<LoginView> {
             decoration:
                 const InputDecoration(hintText: "Enter your password here"),
           ),
-          TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-                try {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {  
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is InvalidCredentialsAuthException) {
+                  await showErrorDialog(context, "Invalid credentials");
+                } else if (state.exception is GenericAuthException) {
+                  await showErrorDialog(context, "Authentication error");
+                }
+              }
+            },
+            child: TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
                   context.read<AuthBloc>().add(
                         AuthEventLogIn(
                           email,
                           password,
                         ),
                       );
-                } on InvalidCredentialsAuthException {
-                  await showErrorDialog(context, "Invalid credentials");
-                } on GenericAuthException {
-                  await showErrorDialog(context, "Authentication error");
-                }
-              },
-              child: const Text("Login")),
+                },
+                child: const Text("Login")),
+          ),
           TextButton(
               onPressed: () {
                 Navigator.of(context)
